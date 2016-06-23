960bf9fa26e81ebadde2d80b2c8c375a3fac5826
FTGStudio <doranm@mail.gvsu.edu>
2016-06-23 19:32:04 -0400

    added display functionality for all 8 bars

--- src/sv_utils/sound_visualizer.h
+++ src/sv_utils/sound_visualizer.h
@@ -1,7 +1,7 @@
 #ifndef __sound_visualizer_h__
 #define __sound_visualizer_h__
 
-#define BUF_SIZE 6400	//0.2/(1/32kHz)
+#define BUF_SIZE 600	//0.2/(1/32kHz)
 
 //Initialization functions
 void Initialize();
@@ -17,5 +17,14 @@ int GetSystemState();
 void UARTSend(const unsigned char *pucBuffer, unsigned long ulCount);
 void RITAddRow(int difference, int prevDisp, int offset);
 void RITClearRow(int difference, int prevDisp, int offset);
+void Display(unsigned long avg, int buf_num);
+void reset_avg_history();
+double get_new_average(double avg);
+void simulate();
+//void get_fft(int buffer_number);
+//void compute_average_magnitude(int buffer_number);
+
+
+
 
 #endif
